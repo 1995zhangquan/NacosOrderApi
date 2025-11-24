d468ec04bf620c8bf6f5dd8df8bd5a7ca4d315f0
张权 <18253626835@163.com>
2025-11-24 16:17:31 +0800

    0.0.2-SNAPSHOT

--- src/main/java/com/cloud/main/NacosOrderApiApplication.java
+++ src/main/java/com/cloud/main/NacosOrderApiApplication.java
@@ -1,7 +1,11 @@
 package com.cloud.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 //开启hystrix支持
 @EnableHystrix
